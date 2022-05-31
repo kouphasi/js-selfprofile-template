991da1f3db34a8643478fcaaa103bd787d2488bf
phasi <koumiyom4@gmail,com>
2022-06-01 00:27:18 +0900

    テンプレートの作

---
+++ main.js
@@ -0,0 +1,8 @@
+//命令通りに動いて、自由度が高い！！
+const display = document.getElementById("change");
+const button = document.getElementById("button");
+
+button.onclick() = () => {
+    //中身の部分を変えよう！！
+    display.textContent = 0;
+}
