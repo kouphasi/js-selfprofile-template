637d424ea559752f2b8063ca8753e3df659ed642
phasi <koumiyom4@gmail,com>
2022-06-10 13:50:50 +0900

    make slide patter

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 //命令通りに動いて、自由度が高い！！
-const display = document.getElementById("change");
+const display = document.getElementById("list");
 const button = document.getElementById("button");
+let count = 0;
+const profile = [
+  "大学:",
+  "趣味:",
+  "好きな食べ物：",
+  "好きな歌手:",
+  "座右の銘:",
+];
 
 button.onclick = () => {
-    //中身の部分を変えよう！！
-    display.textContent = 0;
-}
+  //中身の部分を変えよう！！
+  const li = document.createElement("li");
+  li.textContent = profile[count];
+
+  display.appendChild(li);
+  count += 1;
+};
